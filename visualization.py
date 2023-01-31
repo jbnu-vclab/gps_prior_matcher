20454ddd323793c06969e61a81d3537724dedd32
Dongwon Jeong <jlove981210@gmail.com>
2023-01-31 10:18:29 +0900

    feat: add visualization

---
+++ visualization.py
@@ -0,0 +1,13 @@
+import cv2
+
+def draw_left_right_arrow(img, x1, y1, x2, y2, color=(255,0,0)):
+    cv2.arrowedLine(img, (x1, y1), (x2, y2), color=color, thickness=2)
+    cv2.arrowedLine(img, (x2, y2), (x1, y1), color=color, thickness=2)
+    return img
+
+def draw_matches_pair(img1, img2, keypoints1, keypoints2, matches):
+    res = cv2.drawMatches(img1, keypoints1, img2, keypoints2, matches, None, \
+                        flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+                        # flags=cv2.DRAW_MATCHES_FLAGS_NOT_DRAW_SINGLE_POINTS)
+    res = cv2.resize(res, (0, 0), fx=0.5, fy=0.5)
+    return res
