6902b142d793f422730367df51f4fa42cc8f25b2
Dongwon Jeong <jlove981210@gmail.com>
2023-01-31 10:18:57 +0900

    feat: complete matcher

--- gps_prior_matcher.py
+++ gps_prior_matcher.py
@@ -4,7 +4,10 @@ import argparse
 import sqlite3
 import cv2
 import numpy as np
+from pymap3d import ned
 import collections
+import feature_keypoint as fkp
+import visualization as vis
 
 IS_PYTHON3 = sys.version_info[0] >= 3
 MAX_IMAGE_ID = 2**31 - 1
@@ -19,6 +22,7 @@ def cosine_similarity(vec1: np.array, vec2: np.array) -> float:
 
 def get_adjacent_vector(p1: np.array, p2: np.array) -> np.array:
     return normalize(p2 - p1)
+    # return p2 - p1
 
 def blob_to_array(blob, dtype, shape=(-1,)):
     #! np.fromstring causes deprecation warning(expected to use 'frombuffer') despite using python3
@@ -35,10 +39,15 @@ def array_to_blob(array):
     return np.getbuffer(array)
 
 class GPSPriorMatcher:
+    OVERLAP = 0
+    NO_OVERLAP = 1
+
     def __init__(self):
         self.args = self.parse_args()
         connection = sqlite3.connect(self.args.database_path)
         self.image_path = self.args.image_path
+        self.distance_threshold = self.args.distance_threshold
+        self.overlap = self.args.overlap
         self.cursor = connection.cursor()
         self.read_cameras()
         self.read_images()
@@ -48,6 +57,7 @@ class GPSPriorMatcher:
         self.image_width = self.cameras[0]['width']
         self.image_height = self.cameras[0]['height']
 
+        self.init_matcher()
         self.sort_keypoints_and_descriptors()
     
     def parse_args(self):
@@ -58,6 +68,7 @@ class GPSPriorMatcher:
         parser.add_argument("--min_num_matches", type=int, default=15)
         parser.add_argument("--num_neighbors", type=int, default=10)
         parser.add_argument("--overlap", type=float, default=1.0)
+        parser.add_argument("--distance_threshold", type=float, default=0.60)
         args = parser.parse_args()
         return args
 
@@ -70,6 +81,26 @@ class GPSPriorMatcher:
         if image_id1 > image_id2:
             image_id1, image_id2 = image_id2, image_id1
         return image_id1 * MAX_IMAGE_ID + image_id2
+    
+    def geodetic_to_ned(self, lat:float, lon:float, alt:float) -> list:
+        n, e, d = ned.geodetic2ned(lat, lon, alt, \
+                                  self.observer[0], self.observer[1], self.observer[2])
+        return [n, e, d]
+    
+    def init_matcher(self):
+        FLANN_INDEX_KDTREE = 0
+        index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+        # FLANN_INDEX_LSH = 6
+        # index_params = dict(algorithm = FLANN_INDEX_LSH,
+        #                 table_number = 6,
+        #                 key_size = 12,
+        #                 multi_probe_level = 1
+        #                 )
+        search_params = dict(checks=32,
+                        cross_check=True)
+
+        # self.matcher = cv2.FlannBasedMatcher(index_params, search_params)
+        self.matcher = cv2.BFMatcher()
     
     def read_cameras(self):
         self.cameras = []
@@ -109,6 +140,9 @@ class GPSPriorMatcher:
         self.images = sorted(self.images, key=lambda d: d['image_id'])
         self.last_image_id = len(self.images) - 1
         print(f'Last image ID : {self.last_image_id}')
+
+        self.observer = (self.images[0]['prior_tx'], self.images[0]['prior_ty'], self.images[0]['prior_tz'])
+
         return self.images
 
     #* use 6 col keypoints
@@ -121,10 +155,10 @@ class GPSPriorMatcher:
                     "image_id" : int(row[0]),
                     "row" : row[1],
                     "cols" : row[2],
-                    "data" : blob_to_array(row[3], dtype=np.float32)
+                    "data" : blob_to_array(row[3], dtype=np.float32, shape=(row[1], row[2]))
                 }
             )
-        self.keypoints = sorted(self.keypoints, key=lambda d: d['image_id']) 
+        self.keypoints = sorted(self.keypoints, key=lambda d: d['image_id'])
         return self.keypoints
     
     def read_descriptors(self):
@@ -136,14 +170,29 @@ class GPSPriorMatcher:
                     "image_id" : int(row[0]),
                     "row" : row[1],
                     "cols" : row[2],
-                    "data" : blob_to_array(row[3], dtype=np.uint8)
+                    "data" : blob_to_array(row[3], dtype=np.uint8, shape=(row[1], row[2]))
                 }
             )
-        self.descriptors = sorted(self.descriptors, key=lambda d: d['image_id']) 
+        self.descriptors = sorted(self.descriptors, key=lambda d: d['image_id'])
         return self.descriptors
     
+    def read_two_view_geometries(self):
+        self.two_view_geometries = []
+        self.cursor.execute("SELECT pair_id, rows, cols, data, config, F, E, H, qvec, tvec FROM two_view_geometries;")
+        for row in self.cursor:
+            self.two_view_geometries.append(
+                {
+                    "pair_id" : int(row[0]),
+                    "row" : row[1],
+                    "cols" : row[2],
+                    "data" : blob_to_array(row[3], dtype=np.uint32, shape=(row[1], row[2])),
+                }
+            )
+        # self.two_view_geometries = sorted(self.two_view_geometries, key=lambda d: d['image_id'])
+        return self.two_view_geometries
+    
     def get_gps_from_image(self, image: dict) -> np.array:
-        return np.array([image['prior_tx'], image['prior_ty'], image['prior_tz']])
+        return np.array(self.geodetic_to_ned(image['prior_tx'], image['prior_ty'], image['prior_tz']))
     
     def get_cos_sim_from_images(self, image_id1: int, image_id2: int) -> float:
         image1 = self.images[image_id1]
@@ -166,55 +215,58 @@ class GPSPriorMatcher:
         ''' sort by x axis
         '''    
         for image_id, keypoint in enumerate(self.keypoints):
-            sorted_pairs = sorted(zip(keypoint, self.descriptors[image_id]), key=lambda d: d[0][0])
-            keypoint, self.descriptors[image_id] = zip(*sorted_pairs)
+            kp = keypoint['data']
+            desc = self.descriptors[image_id]['data']
+
+            sorted_pairs = sorted(zip(kp, desc), key=lambda d: d[0][0])
+            kp, desc = zip(*sorted_pairs)
 
     def match_one_pair(self, image_id1: int, image_id2: int):
         cos_sim = self.get_cos_sim_from_images(image_id1, image_id2)
-
-        print(cos_sim)
+        print(f'Cosine similarity : {cos_sim}')
 
         #* no overlap
         if cos_sim <= 0:
-            return
+            return self.NO_OVERLAP
 
         keypoints1, descriptors1 = self.keypoints[image_id1]['data'], self.descriptors[image_id1]['data']
         keypoints2, descriptors2 = self.keypoints[image_id2]['data'], self.descriptors[image_id2]['data']
 
-        overlap_begin = int((1 - cos_sim) * self.image_width)
+        if 0.0 < cos_sim < 0.1:
+            cos_sim = 0.1
+        overlap_begin = int((1 - cos_sim) * self.image_width / 2)
         overlap_end = self.image_width - overlap_begin
 
-        overlap_index1 = np.where(overlap_begin < np.array(keypoints1) < overlap_end)
-        overlap_index2 = np.where(overlap_begin < np.array(keypoints2) < overlap_end)
+        overlap_indices1 = [i for i, kp in enumerate(keypoints1) if overlap_begin < kp[0] < overlap_end]
+        overlap_indices2 = [i for i, kp in enumerate(keypoints2) if overlap_begin < kp[0] < overlap_end]
 
-        overlap_keypoints1 = [kp for i, kp in enumerate(keypoints1) if i in overlap_index1]
-        overlap_keypoints2 = [kp for i, kp in enumerate(keypoints2) if i in overlap_index2]
-        overlap_descriptors1 = [kp for i, kp in enumerate(descriptors1) if i in overlap_index1]
-        overlap_descriptors2 = [kp for i, kp in enumerate(descriptors2) if i in overlap_index2]
+        overlap_keypoints1 = np.array([kp for i, kp in enumerate(keypoints1) if i in overlap_indices1])
+        overlap_keypoints2 = np.array([kp for i, kp in enumerate(keypoints2) if i in overlap_indices2])
+        overlap_descriptors1 = np.array([desc for i, desc in enumerate(descriptors1) if i in overlap_indices1])
+        overlap_descriptors2 = np.array([desc for i, desc in enumerate(descriptors2) if i in overlap_indices2])
 
-        FLANN_INDEX_KDTREE = 1
-        index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
-        search_params = dict(checks=50)
+        overlap_keypoints1 = fkp.arrays_to_keypoints(overlap_keypoints1)
+        overlap_keypoints2 = fkp.arrays_to_keypoints(overlap_keypoints2)
+        
+        matches = self.matcher.knnMatch(overlap_descriptors1, overlap_descriptors2, 2)
 
-        # Flann 매처 생성 ---③
-        matcher = cv2.FlannBasedMatcher(index_params, search_params)
-        # 매칭 계산 ---④
-        matches = matcher.match(overlap_descriptors1, overlap_descriptors2)
+        good_matches = [match[0] for match in matches if match[0].distance / match[1].distance < self.distance_threshold]
 
-        img1 = cv2.imread(self.image_path + '/' + self.image[image_id1]['name'])
-        img2 = cv2.imread(self.image_path + '/' + self.image[image_id2]['name'])
-        # 매칭 그리기
-        res = cv2.drawMatches(img1, overlap_keypoints1, img2, overlap_keypoints2, matches, None, \
-                        flags=cv2.DRAW_MATCHES_FLAGS_NOT_DRAW_SINGLE_POINTS)
+        # img1 = cv2.imread(self.image_path + '/' + self.images[image_id1]['name'])
+        # img1 = vis.draw_left_right_arrow(img1, overlap_begin, 800, overlap_end, 800)
+        # img2 = cv2.imread(self.image_path + '/' + self.images[image_id2]['name'])
+        # img2 = vis.draw_left_right_arrow(img2, overlap_begin, 800, overlap_end, 800)
 
-        cv2.imshow('Test', res)
-        cv2.waitKey()
-        cv2.destroyAllWindows()
+        # res = vis.draw_matches_pair(img1, img2, overlap_keypoints1, overlap_keypoints2, good_matches)
 
-        print('fdsfsdfsdfsdf')
+        # cv2.imshow(f'{image_id1}_{image_id2}', res)
+        # cv2.waitKey()
+        # cv2.destroyAllWindows()
+
+        return good_matches
 
     def gps_prior_matching(self):
-        self.match_one_pair(30, 34)
+        self.match_one_pair(20, 40)
 
         for image in self.images:
             image_id = image['image_id']
